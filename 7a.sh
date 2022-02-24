61ce29a0d094c1d3b067475fdd296371e95a784b
sandyb230 <98949610+sandyb230@users.noreply.github.com>
2022-02-24 15:28:33 +0530

    Add files via upload

---
+++ 7a.sh
@@ -0,0 +1,18 @@
+set --`who`
+u= $1
+t= $4
+set --`echo $t | tr ":" ""`
+h=$1
+echo "user loged in :$h"
+if [ $h -ge 4 ] && [ $h -lt 12 ]
+then
+echo "good morning:$u"
+elif [ sh -ge 4 ] && [ $h -lt 16 ]
+then
+echo "good Afternoon;$u"
+elif [ $ -ge 16 ] && [ $h -t 19 ]
+then
+echo "good Evening:$u"
+else 
+echo "good night:$u"
+fi
