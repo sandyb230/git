b295264e7013ae60dac7d9e8fcfc6b5f788f9e26
sandyb230 <98949610+sandyb230@users.noreply.github.com>
2022-02-25 14:48:35 +0530

    unix version5

--- 5a.sh
+++ 5a.sh
@@ -1,5 +1,7 @@
+#write a shell script that accepts filename as argument and display its creation time if file
+#exit and if doesnot send output error message.
 filename=$1
-if [ -f filename ]
+if [ -e $filename ]
 then
 set -- `ls -ld $filename`
 echo "creation time of  $filename is $6 $7 $8"
