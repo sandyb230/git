68395c0b0c05a484256ac48f88903aaeecd6b5eb
sandyb230 <98949610+sandyb230@users.noreply.github.com>
2022-03-14 16:14:16 +0530

    humming code

--- c3.c
+++ c3.c
@@ -1,62 +1,32 @@
 #include<stdio.h>
-#include<ctype.h>
-char infix[30],postfix[30];
-int top=-1;
-char stack[30];
-void push(char ch)
+void main()
 {
-stack[++top]=ch;
+int data[10],i,c,c1,c2,c3,rec[10];
+printf("enter a message:\n");
+scanf("%d%d%d%d",&data[0],&data[1],&data[2],&data[4]);
+data[5]=data[0]^data[1]^data[4];
+data[6]=data[4]^data[2]^data[0];
+data[3]=data[2]^data[1]^data[0];
+printf("encoded bits are");
+for(i=0;i<7;i++)
+{
+printf("%d",data[i]);
 }
-char pop()
+printf("enter encoded bits");
+for(i=0;i<7;i++)
 {
-return stack[top--];
+scanf("%d",&rec[i]);
 }
-int precedence(int x)
+c1=rec[6]^rec[4]^rec[2]^rec[0];
+c2=rec[5]^rec[4]^rec[1]^rec[0];
+c3=rec[3]^rec[2]^rec[1]^rec[0];
+c=c3*4+c2*2+c1*1;
+if(c==0)
 {
-if(x=='(')
-return 0;
-if(x=='+'||x=='-')
-return 1;
-if(x=='*'||x=='/')
-return 2;
-if(x=='^')
-return 3;
-return 0;
-}
-void infixTopostfix()
-{
-int i=0,k=0;
-char ch;
-while((ch=infix[i++])!='\0')
-{
-if(ch=='(')
-push(ch);
-else if(isalnum(ch))
-postfix[k++]=ch;
-else if(ch==')')
-{
-while(stack[top]!='(')
-postfix[k++]=pop();
-ch=pop();
+printf("there is no error");
 }
 else
 {
-while(precedence(stack[top])>=precedence(ch))
-postfix[k++]=pop();
-push(ch);
+printf("error in position %d/n" ,c);
 }
 }
-while(stack[top]!='$')
-postfix[k++]=pop();
-postfix[k];'\0';
-}
-void main()
-{
-printf("Enter the infix expression");
-scanf("%s",infix);
-push('$');
-infixTopostfix();
-printf("enter the postfix expression is %s",postfix);
-}
-
-
