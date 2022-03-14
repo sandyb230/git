6b6338f08ec6c150ac2bf9c20f445073c2cab035
sandyb230 <98949610+sandyb230@users.noreply.github.com>
2022-03-14 16:15:12 +0530

    buffer

--- c4.c
+++ c4.c
@@ -12,17 +12,21 @@ while(n!=0)
 {
 printf("enter the incoming packets\n");
 scanf("%d",&incoming);
+printf("incomming packet size %d\n",incoming);
 if(incoming<=(buff_size-store))
 {
 store+=incoming;
+printf("bucket buffer size %d out of %d\n",store,buff_size);
 }
 else
 {
 result=(incoming-(buff_size-store));
 printf("dropped packet  %d:\n",result);
+printf("Bucket buffer size %d out of %d\n",store,buff_size);
 store=buff_size;
 }
 store=store-outgoing;
+printf("after out going %d packets out of %d buffer\n",store,buff_size);
 n--;
 }
 }
